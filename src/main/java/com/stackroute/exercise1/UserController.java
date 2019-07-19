bea1037c962a276b7a0a64dba1ef6bfaec767aa8
nagapremika <51943079+nagapremika@users.noreply.github.com>
2019-07-19 10:49:20 +0530

    Update UserController.java

--- src/main/java/com/stackroute/exercise1/UserController.java
+++ src/main/java/com/stackroute/exercise1/UserController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class UserController {
+public class UserController {       //Controller
 
 
-    @RequestMapping("/")
+    @RequestMapping("/")        //Handles the request mapping with "/"
     public String greet(Model model){
         User user = new User("Premika");
-        model.addAttribute("message",user.getMessage());
+        model.addAttribute("message",user.getMessage());        //prints the message
         return "index";
     }
 
